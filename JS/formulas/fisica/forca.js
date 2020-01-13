7bf104e426ae3cc3c8665465fc54959820344a11
Astrobio00 <59454048+Astrobio00@users.noreply.github.com>
2020-01-13 20:17:52 -0300

    Segunda versão do Site
    Aqui já foram disponibilizadas duas fórmulas e a página inicial.

---
+++ JS/formulas/fisica/forca.js
@@ -0,0 +1,29 @@
+function calcular() {
+    var aValue = window.document.getElementById('aValue')
+    var bValue = window.document.getElementById('bValue')
+    var cValue = window.document.getElementById('cValue')
+    var result = 0
+
+    if (aValue.value == "" || aValue.value == 0) {
+        result = (Number.parseInt(bValue.value) * Number.parseInt(cValue.value))
+        aValue.value = (result)
+        cValue.style.backgroundColor = 'white'
+        bValue.style.backgroundColor = 'white'
+        aValue.style.backgroundColor = 'green'
+    }
+
+    if (bValue.value == "" || bValue.value == 0) {
+        result = (Number.parseInt(aValue.value) / Number.parseInt(cValue.value))
+        bValue.value = (result)
+        cValue.style.backgroundColor = 'white'
+        bValue.style.backgroundColor = 'green'
+        aValue.style.backgroundColor = 'white'
+    }
+    if (cValue.value == "" || cValue.value == 0) {
+        result = (Number.parseInt(aValue.value) / Number.parseInt(bValue.value))
+        cValue.value = (result)
+        cValue.style.backgroundColor = 'green'
+        bValue.style.backgroundColor = 'white'
+        aValue.style.backgroundColor = 'white'
+    }
+}
